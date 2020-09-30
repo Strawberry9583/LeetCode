574da3bf806cee6de17345674b2f84b0a58fe323
Xia_Hai <76794087@qq.com>
2020-09-30 15:35:52 +0800

    first missing positive

---
+++ Challenge/September_LeetCoding_Challenge/Day2_contains_duplicate_III/Day2_contains_duplicate_III/Day2_contains_duplicate_III.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<queue>
+
+
+using namespace std;
+
+
+class Solution {
+public:
+	bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+		std::priority_queue<int,int>
+	}
+};
+
+int main() {
+	cin.get();
+	return 0;
+}
